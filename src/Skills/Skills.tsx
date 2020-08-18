fc161dae1cc0d3049b9b1721319ce98462d3f305
Travis Anderson <thetanderson@gmail.com>
2020-08-18 17:48:36 -0400

    push test

--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 
-const Skills = () => {
-  return (
-    <React.Fragment>
-      <h1 id="skills">SKILLS</h1>
-      <i>React</i>
-      <i>JavaScript</i>
-      <i>Node</i>
-      <i>TypeScript</i>
-      <i>Python</i>
-      <i>SQL</i>
-      <i>Scala</i>
-      <i>Django</i>
-    </React.Fragment>
-  );
-};
+const Skills = () => (
+  <>
+    <h1 id="skills">SKILLS</h1>
+    <i>React</i>
+    <i>JavaScript</i>
+    <i>Node</i>
+    <i>TypeScript</i>
+    <i>Python</i>
+    <i>SQL</i>
+    <i>Scala</i>
+    <i>Django</i>
+    <i>More</i>
+  </>
+);
 
 export default Skills;
